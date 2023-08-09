cf4f7670446159e7522e5f183198bbdc59730897
nikolasfil <nikolas13in@gmail.com>
2023-08-09 22:52:05 +0300

    update

---
+++ Lab03/paradoteo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zip paradoteo.zip question01/question_01.jff question02/question_02.jff question03/question_03.jff question04/question_04.jff question05/question_05.jff question05/question_05_cnf.jff report.pdf
